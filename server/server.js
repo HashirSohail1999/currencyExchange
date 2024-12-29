b2d4bd154f2e63cb76e6ecd7929e820178e7440b
Hashir <syedhashirsohail@gmail.com>
2024-12-29 13:41:58 +0500

    checking env

--- server/server.js
+++ server/server.js
@@ -1,9 +1,19 @@
+require('dotenv').config();
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Access your API keys using process.env
+const apiKey = process.env.FREECURRENCYAPI_KEY;
+const freecurrencyapi = new Freecurrencyapi(apiKey);
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+    freecurrencyapi.latest({
+        base_currency: 'USD',
+        currencies: 'EUR'
+    }).then(response => {
+        console.log(response);
+        res.send(response+' Hello World!');
+    });
 });
 
 app.listen(port, () => {
